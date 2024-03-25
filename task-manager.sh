2fffe0eb3a8784c8ac8d261c9210ded8b3f570a2
retif <retif@decovar.dev>
2024-03-25 11:06:23 +0100

    Timestamps in the output, timing the script run

--- task-manager.sh
+++ task-manager.sh
@@ -12,12 +12,15 @@ export VULCAN_RUNS_ARE_CHAINED=1
 # if n=1 and n<3, then it means 2 times (3 - 1 = 2)
 for ((n=1; n<3; n++)); do
     export VULCAN_RUN_ORDINAL_NUMBER=${n}
-    echo -e "\n[INFO] VULCAN run #${VULCAN_RUN_ORDINAL_NUMBER}\n"
-    python ./vulcan.py
+
+    echo -e "\n[INFO] [$(date '+%Y-%m-%d %H:%M:%S')] VULCAN run #${VULCAN_RUN_ORDINAL_NUMBER}\n"
+
+    time python -u ./vulcan.py
+
     if [ $? -eq 0 ]; then
-        echo -e "\n[INFO] Done executing VULCAN run #${VULCAN_RUN_ORDINAL_NUMBER}\n"
+        echo -e "\n[INFO] [$(date '+%Y-%m-%d %H:%M:%S')] Done executing VULCAN run #${VULCAN_RUN_ORDINAL_NUMBER}\n"
     else
-        echo "[ERROR] The VULCAN run #${VULCAN_RUN_ORDINAL_NUMBER} failed" >&2
+        echo "[ERROR] [$(date '+%Y-%m-%d %H:%M:%S')] The VULCAN run #${VULCAN_RUN_ORDINAL_NUMBER} failed" >&2
         exit 2
     fi
 done
